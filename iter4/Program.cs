67335d4f6c97f25a61b748452ce0c409619f10d0
Elena <keaya@rambler.ru>
2022-08-05 23:54:59 +0300

    Программа вывода последней цифры трехзначного числа

---
+++ iter4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа вывода последней цифры трехзначного числа");
+
+Console.WriteLine("Введите число");
+
+string numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+int i = number - ((number / 10) * 10);
+
+Console.WriteLine("Последняя цифра введенного числа " + i);
