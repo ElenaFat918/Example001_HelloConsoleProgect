fe9f74e1bea79dbe926839ed942bb808932c788a
Elena <keaya@rambler.ru>
2022-08-05 23:59:41 +0300

    Замена оператором %10

--- iter4/Program.cs
+++ iter4/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Программа вывода последней цифры трехзначного числа");
+﻿Console.WriteLine("Программа вывода последней цифры числа");
 
 Console.WriteLine("Введите число");
 
@@ -6,6 +6,6 @@ string numberString = Console.ReadLine();
 
 int number = int.Parse(numberString);
 
-int i = number - ((number / 10) * 10);
+int i = number %10;
 
 Console.WriteLine("Последняя цифра введенного числа " + i);
