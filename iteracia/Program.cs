fec4fa235b959012b8880c704a5eff0d9bd9deba
Elena <keaya@rambler.ru>
2022-08-05 22:59:09 +0300

    Добавили проект проверки является ли первое число квадратом второго

---
+++ iteracia/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Программа проверки ");
+
+Console.WriteLine("Введите первое число");
+
+string numberString = Console.ReadLine();
+
+int numberA = int.Parse(numberString);
+
+Console.WriteLine("Введите второе число");
+
+string numberStringA = Console.ReadLine();
+
+int numberB = int.Parse(numberStringA);
+
+int sgrt = numberA * numberA;
+
+if(numberA == sgrt)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
