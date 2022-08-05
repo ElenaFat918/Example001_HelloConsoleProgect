b7c19c3cb5db598d176cde99eccad18f1e67cabf
Elena <keaya@rambler.ru>
2022-08-05 23:42:25 +0300

    Программа вывода всех целых чисел от -N до N

---
+++ iter3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа вывода всех целых чисел от -N до N");
+
+Console.WriteLine("Введите число");
+
+string numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+if(number < 0)
+
+number = number * (-1);
+
+int i = number * (-1);
+
+while (i <= number)
+
+Console.WriteLine(i++);
