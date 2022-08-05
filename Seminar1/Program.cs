bea551a5abc704ce6bc66cf536aaf257e4749909
Elena <keaya@rambler.ru>
2022-08-05 22:57:42 +0300

    Добавили проект возведения числа в квадрат

---
+++ Seminar1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа возведения числа в квадрат");
+
+Console.WriteLine("Программа возведения числа в квадрат");
+
+string numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+int sgrt = number * number;
+
+Console.WriteLine("Квадрат числа" + number + "есть число" + sgrt);
