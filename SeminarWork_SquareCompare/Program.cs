3fef6c8e07a90eae7099703e0a750c643eaf3ac4
Elena <keaya@rambler.ru>
2022-08-09 22:36:04 +0300

    Программа принятия на вход 2х чисел и проверки, является ли одно число квадратом другого

---
+++ SeminarWork_SquareCompare/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принятия на вход 2х чисел и проверки, является ли одно число квадратом другого
+Console.WriteLine("Первое число");
+string? numberString1 = Console.ReadLine();
+int number1 = int.Parse(numberString1);
+
+Console.WriteLine("Второе число");
+string? numberString2 = Console.ReadLine();
+int number2 = int.Parse(numberString2);
+
+int sqrt1 = number1 * number1;
+int sqrt2 = number2 * number2;
+
+if (sqrt2 == number1 || sqrt1 == number2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
