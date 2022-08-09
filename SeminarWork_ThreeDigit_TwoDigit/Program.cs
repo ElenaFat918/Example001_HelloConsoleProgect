e33309336b466a7d1a8ed4b6217caf379382cb25
Elena <keaya@rambler.ru>
2022-08-09 22:15:37 +0300

    Программа вывода случайного трехзначного числа и удаления второй цифры этого числа

---
+++ SeminarWork_ThreeDigit_TwoDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа вывода случайного трехзначного числа и удаления второй цифры этого числа
+
+Random random = new Random();
+
+int randomNumber = random.Next(100, 1000);
+
+Console.WriteLine(randomNumber);
+
+int firstnumber = randomNumber / 100; // Получаем первую цифру
+
+int secondnumber = randomNumber % 10; // Получаем последнюю
+
+int thirdnumber = firstnumber * 10; // Превращем первое число в двухзначное
+
+int finish =  thirdnumber + secondnumber; // Складываем двухзначное и последнее
+
+Console.WriteLine(finish);
