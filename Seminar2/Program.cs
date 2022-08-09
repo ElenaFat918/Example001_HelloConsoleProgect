ff5d585f60ad40be209b12923a1099f052605732
Elena <keaya@rambler.ru>
2022-08-09 20:34:22 +0300

    случ число из отрезка [10,99] и показывает наибольш цифру числа

---
+++ Seminar2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать прог, кот выводит случ число из отрезка х10б99ъ и показывает наибольш цифру числа
+
+Random random = new Random();
+
+int randomNumber = random.Next(10,99);
+
+Console.WriteLine(randomNumber);
+
+int firstDigit = randomNumber % 10;
+
+int secondDigit = randomNumber / 10;
+if (firstDigit > secondDigit)
+    Console.WriteLine(firstDigit);
+else 
+    Console.WriteLine(secondDigit);
