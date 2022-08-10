e46ed38700048dd97f6aa4df8b0bfb78a25f1bc5
Elena <keaya@rambler.ru>
2022-08-10 19:40:19 +0300

    Программа принятия на вход трёхзначного числа и вывода второй цифры этого числа

---
+++ HomeWork_SecondFigure/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа принятия на вход трёхзначного числа и вывода второй цифры этого числа
+
+Console.WriteLine("Введите трёхзначное число");
+
+string numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+int firstFigure = (number / 100) * 100; // Получаем cотню
+
+int secondFigure = (number -  firstFigure) / 10; // убираем сотню и десяток
+
+Console.WriteLine(secondFigure);
