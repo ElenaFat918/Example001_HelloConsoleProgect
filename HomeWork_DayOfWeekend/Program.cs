7d7847d55922ec200dd467208b58203d293ad24e
Elena <keaya@rambler.ru>
2022-08-10 20:36:27 +0300

    Программа принятия на вход цифры, обозначающей день недели и проверки соответствия выходному дню

---
+++ HomeWork_DayOfWeekend/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа принятия на вход цифры, обозначающей день недели и проверки соответствия выходному дню
+
+Console.WriteLine("Программа проверки соответствия цифре выходному дню");
+
+Console.WriteLine("Введите номер дня недели");
+
+string numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+if (number > 0 && number < 6)
+{
+    Console.WriteLine("нет");
+}
+else if(number > 5 && number < 8)
+{
+   Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели нет, введите пожалуйста число от 1 до 7 включительно");
+}    
