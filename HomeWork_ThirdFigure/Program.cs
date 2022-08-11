f911a724cee1f771820287a73aabbf9e126c2850
Elena <keaya@rambler.ru>
2022-08-11 20:13:16 +0300

    Ввела условие для чисел, больше или равно 1000

--- HomeWork_ThirdFigure/Program.cs
+++ HomeWork_ThirdFigure/Program.cs
@@ -9,6 +9,10 @@ string numberString = Console.ReadLine();
 
 int number = int.Parse(numberString);
 
+while(number >= 1000)
+{
+    number = number/10;
+}
 
 if(number > 0 && number < 100)
 
@@ -20,30 +24,4 @@ else if(number > 99 && number < 1000)
 {   int firstFigure = (number / 10) * 10; // Получаем cотню
     int secondFigure = number -  firstFigure; // убираем сотню и десяток
     Console.WriteLine(secondFigure);
-}
-
-
-else if(number > 999 && number < 10000)
-
-{   int firstFigure = (number / 100) * 100; // Получаем 1000-ную
-    int secondFigure = (number -  firstFigure)/10; // убираем сотню и десяток
-    Console.WriteLine(secondFigure);
-}
-
-
-else if(number > 9999 && number < 100000)
-{   int firstFigure = (number / 1000) * 1000; // Получаем 10000-ную
-    int secondFigure = (number -  firstFigure)/100; // убираем сотню и десяток
-    Console.WriteLine(secondFigure);
-}
-
-
-else if(number > 99999 && number < 1000000)
-{   int firstFigure = (number / 10000) * 10000; // Получаем 10000-ную
-    int secondFigure = (number -  firstFigure)/1000; // убираем сотню и десяток
-    Console.WriteLine(secondFigure);
-}
-else
-{
-    Console.WriteLine("Введенное число не предусмотрено программой, пожалуйста обратитесь к разработчику");
 }
