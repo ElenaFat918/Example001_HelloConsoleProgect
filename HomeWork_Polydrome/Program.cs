58399f738614d70d955be64621656ab4abe6413d
Elena <keaya@rambler.ru>
2022-08-13 22:43:05 +0300

    Программа проверки пятизначного числа на полиндром

---
+++ HomeWork_Polydrome/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+Console.WriteLine("Программа проверки пятизначного числа на полиндром");
+Console.WriteLine("Введите пятизначное число:");
+var String = Console.ReadLine();
+int Number = int.Parse(String);
+
+if (Number < 10000 && Number > 99999)
+{
+    Console.WriteLine("Ошибка, введенное число непятизначное");
+    return;
+}
+
+int Number1 = Number / 10000;
+int Number2 = Number / 1000 - Number1 * 10;
+int Number4 = (Number - (Number / 100 * 100)) / 10;
+int Number5 = (Number - (Number / 10 * 10));
+
+if(Number1 == Number5 && Number2 == Number4)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
