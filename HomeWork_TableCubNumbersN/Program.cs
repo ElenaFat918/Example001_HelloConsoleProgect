9b067525f12077d736908fdb2afd23c938c68824
Elena <keaya@rambler.ru>
2022-08-13 23:12:37 +0300

    Таблица кубов чисел от 1 до N

---
+++ HomeWork_TableCubNumbersN/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+
+Console.Clear();
+
+Console.WriteLine("Таблица квадратов чисел от 1 до N");
+Console.WriteLine("Введите N:");
+
+int N = int.Parse(Console.ReadLine()!);
+
+if(N<1)
+{
+    Console.WriteLine("Ошибка, N должно быть больше 0");
+    return;
+}
+int i = 1;
+
+while(N >= i)
+{
+     double A = Math.Pow(i, 3);
+    Console.Write(A + " ");
+    i++;
+}
