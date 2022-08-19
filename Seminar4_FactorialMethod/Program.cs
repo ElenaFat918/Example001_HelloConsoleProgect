7c491ba5cc81ffa36d67fa84063b8903678ea31f
Elena <keaya@rambler.ru>
2022-08-19 15:21:28 +0300

    Программа, которая принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ Seminar4_FactorialMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿// Решение в группах задач:
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+long Factorial (long num)
+{
+    var result = 1L;  
+    for(long i = 1; i <= num; i ++)
+    {
+        result *= i;
+    }
+    
+    return result;
+}
+
+Console.WriteLine("Программа, которая принимает на вход число N и выдаёт произведение чисел от 1 до N");
+Console.WriteLine("Введите ваше число: ");
+if (long.TryParse(Console.ReadLine()!, out var number))
+{
+    if (number < 0)
+        return;
+    var result = Factorial(number);
+
+    Console.WriteLine(result);
+
+} 
+else {
+    Console.WriteLine("Введено не число или слишком большое число!");
+}
