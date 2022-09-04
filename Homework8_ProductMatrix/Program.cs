78c92e71953e96e0208fdc9fcb6db938c176a7a5
Elena <keaya@rambler.ru>
2022-09-05 01:49:32 +0300

    Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц

---
+++ Homework8_ProductMatrix/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+Console.WriteLine($"Программа поиска произведения двух матриц");
+Console.WriteLine($"\nЗадайте две матрицы. Введите размеры матриц и верхнюю границу создаваемого случайного числа:");
+int m = InputNumbers("Введите число строк первой матрицы: ");
+int n = InputNumbers("Введите число столбцов первой матрицы (и строк второй): ");
+int p = InputNumbers("Введите число столбцов второй матрицы: ");
+int maxValue = InputNumbers("Введите верхнюю границу создаваемого случайного числа:");
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m, p];
+
+ProductMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintArray(resultMatrix);
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ProductMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int product = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                product += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = product;
+        }
+    }
+}
