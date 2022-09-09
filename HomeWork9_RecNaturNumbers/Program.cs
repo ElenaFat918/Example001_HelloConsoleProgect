96c5dc62a41559f0aa0a6f0635b13ba39ee6f5d2
Elena <keaya@rambler.ru>
2022-09-09 07:42:32 +0300

    Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N

---
+++ HomeWork9_RecNaturNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+void PrintRecNaturNumbers(int m, int n)
+
+{
+    if (m == n) Console.Write($"{n} ");
+    else if (m < n)
+    {
+        Console.Write($"{m} ");
+        PrintRecNaturNumbers(m + 1, n);
+    }
+
+    else
+    {
+        Console.Write($"{m} ");
+        PrintRecNaturNumbers(m - 1, n);
+    }
+
+}
+
+int m = 6;
+int n = 3;
+PrintRecNaturNumbers(m, n);
